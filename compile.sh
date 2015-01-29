afccb6543809f23319f0e873248b61b0132c40bb
root <adrian@databank.ro>
2015-01-29 18:47:11 -0500

    compile.sh

---
+++ compile.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+PWD=pwd
+
+cd /tmp
+wget http://ro1.php.net/get/php-5.6.5.tar.gz/from/this/mirror -O php-5.6.5.tar.gz
+rm -rf php-5.6.5/
+tar zxvf php-5.6.5.tar.gz
+
+cd php-5.6.5/
+
+./configure --prefix=/tmp/php-5.6.5/compiled/	\
+	--disable-all	\
+	--without-pear	\
+	--enable-shared=no	\
+	--enable-static=yes	\
+	--enable-phar	\
+	\
+	--with-openssl	\
+	--with-curl	\
+	\
+	--with-mhash	\
+	\
+	--with-gd	\
+	--with-freetype-dir	\
+	\
+	--enable-mbstring	\
+	\
+	--enable-sockets
+
+make
+make install 
+
+cd $PWD
+
+
