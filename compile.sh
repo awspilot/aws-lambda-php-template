96c98acfa75f1bca462ae2a7cfddd59c0d991ddc
root <root@ip-172-31-5-73.eu-central-1.compute.internal>
2015-07-21 19:00:35 +0000

    binary with added SimpleXML and libjpeg suport for GD

--- compile.sh
+++ compile.sh
@@ -2,6 +2,8 @@
 
 PWD=pwd
 
+yum install -y libexif-devel libjpeg-devel gd-devel curl-devel openssl-devel libxml2-devel
+
 cd /tmp
 wget http://ro1.php.net/get/php-5.6.5.tar.gz/from/this/mirror -O php-5.6.5.tar.gz
 rm -rf php-5.6.5/
@@ -10,19 +12,24 @@ tar zxvf php-5.6.5.tar.gz
 cd php-5.6.5/
 
 ./configure --prefix=/tmp/php-5.6.5/compiled/	\
-	--disable-all	\
 	--without-pear	\
 	--enable-shared=no	\
 	--enable-static=yes	\
 	--enable-phar	\
 	--enable-json	\
 	\
+	--disable-all	\
 	--with-openssl	\
 	--with-curl	\
+	\
+	--enable-libxml	\
+	--enable-simplexml	\
+	--enable-xml	\
 	\
 	--with-mhash	\
 	\
 	--with-gd	\
+	--enable-exif	\
 	--with-freetype-dir	\
 	\
 	--enable-mbstring	\
